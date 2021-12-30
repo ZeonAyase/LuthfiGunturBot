142bfccc179d5b8cbaf0964379f87ea34264de2e
ZeonAyase <95622851+ZeonAyase@users.noreply.github.com>
2021-12-30 16:02:01 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,55 +10,78 @@ const token = '5044414524:AAGXkue7mN0oXmPgKRgm0Mgzc2r1N6Udeuw'
 const bot = new TelegramBot(token, {polling: true});
 
 
-// Main Menu Bot
-bot.content.onText(/\/star/, (msg) => {
-    console.log(msg)
+// bots
+let state = 2;
+bot.onText(/\/start/, (msg) => {    
+    state = 0;
     bot.sendMessage(
         msg.chat.id,
-        `Hello $(msg.chat.first_name), welcome...\n
-        click/predict`
-    );
+        `Selamat datang di LuthfiGunturBOT. 
+        \nSilahkan pilih menu dibawah ini:\n
+        (/1) Prediksi dengan Input x1|x2|x3
+        (/2) Batal`
+    ); 
+    bot.sendMessage(msg.chat.id, "Pilihan Anda: ");    
 });
 
-//input requires i and v 
-state = 0;
-bot.onText(/\/predict/,(msg) => {
-    bot.sendMessage(
-        msg.chat.id,
-        'Masukan nilai x1|x2|x3 contohnya 30|88|122`
-    );
+bot.onText(/\/1/, (msg) => {
     state = 1;
+    bot.sendMessage(
+        msg.chat.id, 
+        `Masukan nilai x1, x2, dan x3
+        contohnya: 30|88|122 `
+    );   
+});
+
+bot.onText(/\/2/, (msg) => {
+    state = 2;
+    bot.sendMessage(
+        msg.chat.id, 
+        "pilih /start untuk kembali ke menu utama"
+    );   
 });
 
 bot.on('message', (msg) => {
+    const text = msg.text.toString().toLowerCase();
+    console.log(text);
+
     if(state == 1){
-        s = msg.text.split("|");
-        x1 = s[0]
-        x2 = s[1]
-        x3 = s[2]
+        let dt = text.split('|');
+        x1 = dt[0]
+        x2 = dt[1]
+        x3 = dt[2]
+        bot.sendMessage(
+            msg.chat.id, 
+            `prediksi n x1 (${dt[0]} Volt), x2 (${dt[1]} dan x3 (${dt[2]}) `
+        );
+
         model.predict(
             [
-                parseFloat(s[0]), // string to  float
-                parseFloat(s[1]),
-                parseFloat(s[2])
+                parseFloat(dt[0]), // string to float
+                parseFloat(dt[1]),
+                parseFloat(dt[2])
             ]
-        ).then((jres)=>{
+        ).then((jres) => {
             bot.sendMessage(
-                msg.chat.id,
-                `Nilai nx1 yang diprediksi adalah $(jres[0]) Volt`
+                msg.chat.id, 
+                `nilai x1 , x2, dan x3 adalah (${jres[0]} volt), (${jres[1]} volt), dan (${jres[2]} volt)`
             );
             bot.sendMessage(
                 msg.chat.id,
-                `Nilai nx2 yang diprediksi adalah $(jres[1]) Volt`
+                `<= kembali /2`
             );
-            bot.sendMessage(
-                msg.chat.id,
-                `Nilai nx3 yang diprediksi adalah $(jres[2]) Volt`
-        })
-    }else{
-        state = 0
+        });        
+    }
+
+    if(state == 2){
+        bot.sendMessage(
+            msg.chat.id, 
+            "pilih /start untuk ke menu utama"
+        );   
     }
 })
+
+
 
 // routers
 r.get('/prediction/:x1/:x2/:x3', function(req, res, next) {    
